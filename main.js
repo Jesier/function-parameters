5679d549f2cdec855a154c416ea82c772884c008
Jesier <jesiersantiago@gmail.com>
2022-10-07 10:26:08 -0500

    push

---
+++ main.js
@@ -0,0 +1,11 @@
+/*make function
+task it to divide by four
+store the value in a variable*/
+
+//my function
+const quarterValue = (myNum) => {
+    const quarter = myNum / 4
+        return quarter
+};
+const functionResult = quarterValue(80)
+console.log(functionResult)
